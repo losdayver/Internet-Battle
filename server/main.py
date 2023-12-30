c5e18c8d231bda991cd817906c50d7975b14f189
nist <nist.dev@yandex.ru>
2023-12-30 12:00:27 +0300

    добавил несколько комнат

--- server/main.py
+++ server/main.py
@@ -12,22 +12,22 @@ class Communicator:
         self.communicationPort = 5888
         self.packetSize = 1024
         self.gameSessions = {}
-        self.sessionPID = 0
-        # TODO мб через несколько сокетов
         self.senderQueue = multiprocessing.Manager().Queue()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(("", self.communicationPort))
 
     def createSession(self):
         q = multiprocessing.Manager().Queue()
-        session = Session(q, self.senderQueue)
+        playersCount = multiprocessing.Manager().list()
+        playersCount.append(0)
+        session = Session(q, self.senderQueue, playersCount)
         p = multiprocessing.Process(target=session.simulate)
-        self.gameSessions[p.pid] = q
-        self.sessionPID = p.pid
         p.start()
+        self.gameSessions[p.pid] = [q, playersCount, []]
+        return p.pid
 
-    def run(self):  # TODO мб select
-        self.createSession()
+    def run(self):
+        #self.createSession()
 
         p = multiprocessing.Process(target=self.sender)
         p.start()
@@ -49,12 +49,37 @@ class Communicator:
 
     def commandHandler(self, content, addr):
         packetDict = json.loads(content)
-        self.gameSessions[self.sessionPID].put([packetDict, addr])
+        if packetDict["type"] == "connection" and packetDict["action"] == "connect":
+            sessionFound = False
+
+            for sessionId, data in self.gameSessions.items():
+                if data[1][0] < 4:
+                    self.gameSessions[sessionId][0].put([packetDict, addr])
+                    self.gameSessions[sessionId][2].append(addr)
+                    sessionFound = True
+                    break
+
+            if not sessionFound:
+                newSessionPid = self.createSession()
+                self.gameSessions[newSessionPid][0].put([packetDict, addr])
+                self.gameSessions[newSessionPid][2].append(addr)
+        else:
+            for sessionId, data in self.gameSessions.items():
+                if addr in data[2]:
+                    self.gameSessions[sessionId][0].put([packetDict, addr])
+                    break
 
     def sender(self):
         while True:
             try:
                 addrs, packet = self.senderQueue.get(False)
+                if "delete" in packet.keys():
+                    addr = packet["delete"]
+                    for sessionId, data in self.gameSessions.items():
+                        if addr in data[2]:
+                            self.gameSessions[sessionId][2].remove(addr)
+                            break
+                    return
                 packet = json.dumps(packet)
 
                 for addr in addrs:
